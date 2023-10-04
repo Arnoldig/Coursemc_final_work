6d85434569203179783b449f27d461f754997b49
Arnoldig <codergit@yandex.ru>
2023-10-04 23:04:54 +0300

    Переименованы функции Авторизация и Регистрация на укороченные версии. Добавлены типы проверяемых исключений. Реализована функция верификации нового логина на требуемые критерии. Добавлены мелкие правки описания задачи и отмечены реализованные пункты.

--- main.py
+++ main.py
@@ -15,14 +15,14 @@
 #    1) просим Пользователя придумать логин и пароль
 #    2) проверяем и сохряняем полученные данные
 #       - логин:
-#         - длина: более 3 и менее 20 (включительно всё)
-#         - символы: допускается использовать только буквы и цифры (нет в ТЗ)
+# +       - длина: более 3 и менее 20 (включительно всё)
+# +       - символы: допускается использовать только буквы и/или цифры (нет в ТЗ)
 #         - уникальность: нового логина не должно быть в файле
-#         - доступность файла: проверяем файл на доступность к записи и существование
 #       - пароль:
 #         - длина: более 4 и менее 32 (включительно)
-#         - простота: пароль должен состоять из букв и хотя бы одной цифры (нет в ТЗ)
+#         - символы: пароль должен состоять из букв и хотя бы одной цифры (нет в ТЗ)
 #         - страховка: вводим новый пароль 2 раза (нет в ТЗ)
+#       - доступность файла: проверяем файл на доступность к записи и существование
 #   3) сообщаем об успешной регистрации или неудаче
 
 # + В) Модуль Авторизация:
@@ -33,16 +33,16 @@
 
 def main():
     while True:
-        answer = input('Введите 1, если вы хотите авторизоваться, и 2 - зарегистрироваться):')
+        answer = input('Введите 1 для авторизации, и 2 для регистрации: ')
         if not answer.isnumeric():
             continue
         elif 1 <= int(answer) <= 2:
-            get_authorization() if int(answer) == 1 else get_registration()
+            get_enter() if int(answer) == 1 else get_register()
             break
 
 
-def get_authorization():
-    print('Вызвана функция 1: авторизация')
+def get_enter():
+    print('Вызвана функция 1: авторизация\n')
     while True:
         if 'user_login' not in locals():
             user_login = input('Ваш логин пожалуйста: ')
@@ -55,14 +55,13 @@ def get_authorization():
         user_paswd = input('Ваш пароль пожалуйста:')
         if user_paswd == '':
             print('Ошибка: введён пустой пароль')
-            continue
         else:
             break
 
     try:
         with open('file_with_login_pass.txt') as file:
             body_file = file.readlines()
-    except:
+    except FileNotFoundError:
         print('Файл с данными не найден. Авторизация не выполнена.')
         return -1
 
@@ -71,20 +70,65 @@ def get_authorization():
             print('Авторизация выполнена успешно!')
             return 1
 
-    else:
-        print('Логин или пароль не найдены или указаны неверно.\n')
-        repeat = input('Напишите 1, если хотите попробовать ввести логин и пароль ещё раз.')
-        try:
-            if int(repeat) == 1:
-                user_login = user_paswd = ''
-                get_authorization()
-        except:
-            return 0
+    print('Логин или пароль не найдены или указаны неверно.\n')
+    repeat = input('Напишите 1, если хотите попробовать ещё раз: ')
+    try:
+        if int(repeat) == 1:
+            get_enter()
+    except (TypeError, ValueError):
+        return 0
 
 
-def get_registration():
-    print('Вызвана функция 2: регистрация')
-    pass
+def get_login_verification(login: str):
+    check_len, chek_num, chek_alf = 0, 0, 0
+    if 3 <= len(login) <= 20:
+        check_len += 1
+        for i in login:
+            if check_len + chek_num + chek_alf >= 2:
+                return 1
+            elif i.isnumeric() and chek_num == 0:
+                chek_num += 1
+            elif i.isalpha() and chek_alf == 0:
+                chek_alf += 1
+    return 0
+
+
+def creat_login():
+    while True:
+        new_login = input(
+            'Придумайте логин подходящий под следующие критерии:\n'
+            '  1) длина от 3 по 20 символов\n'
+            '  2) использованы только буквы и/или цифры\n'
+            'Введите новый логин здесь: ')
+
+        if new_login == '':
+            new_login = input('Попробуйте ещё раз: ')
+            if new_login == '':
+                print('Ошибка: введён пустой логин\n')
+        else:
+            if get_login_verification(new_login) == 1:
+                print('Прекрасный логин!')
+                return 1
+            else:
+                repeat = input('Логин не соответствует нужным критериям.\n'
+                               'Нажмите 1, чтобы попробовать ещё раз: ')
+                try:
+                    if int(repeat) != 1:
+                        break
+                except (TypeError, ValueError):
+                    return 0
+
+def creat_passwd():
+
+
+def get_register():
+    print('Вызвана функция 2: регистрация\n')
+    new_login = creat_login()
+    if new_login == 0:
+        return 0
+    new_paswd = creat_passwd()
+    if new_paswd == 0:
+        return 0
 
 
 if __name__ == '__main__':
