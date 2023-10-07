b0843a5c5004ba7d04cd76a8448c5556e14be1e7
Arnoldig <codergit@yandex.ru>
2023-10-07 21:16:36 +0300

    Устранена ошибка с повторением кода, функция Авторизации разбита на несколько функций, в отдельные функции вынесена работа с файлами, запросом данных пользователя и проверку полученных от него данных. Есть сомнения в части использования функции возвращающей целое числов или список в части обработки подобного результата - пока беру тайм-аут на "подумать".

--- main.py
+++ main.py
@@ -32,9 +32,17 @@
  +     - строчные и заглавные буквы должны отличаться
  +  3) сообщаем об успешной авторизации или неудаче
 """
+from typing import Union
+
+REQUEST_REENTER_DATA = 'Введите 1 для повторного ввода: '
+DATA_FILE = 'file_with_login_pass.txt'
 
 
-def main():
+def main() -> None:
+    """
+    Запуск кода для регистрации или авторизации пользователя
+        :return: ничего не возвращает - только запускает выбранные модуль
+    """
     while True:
         answer = input('Введите 1 для авторизации, и 2 для регистрации: ')
         if answer == '1':
@@ -43,92 +51,149 @@ def main():
         elif answer == '2':
             get_registration()
             break
+    return None
 
 
-def get_authorization():
-    print('Вызвана функция 1: авторизация\n')
-    while True:
-        if 'user_login' not in locals():
-            user_login = input('Ваш логин пожалуйста: ')
-        if user_login == '':
-            user_login = input('Попробуйте ещё раз: ')
-            if user_login == '':
-                print('Ошибка: введён пустой логин')
-                continue
+def get_user_data(displ_user_text: str) -> str:
+    """Запрашивает у пользователя данные"""
+    user_answr = input(f'{displ_user_text}: ')
+    return user_answr
 
-        user_paswd = input('Ваш пароль пожалуйста: ')
-        if user_paswd == '':
-            print('Ошибка: введён пустой пароль')
-        else:
-            break
 
+def processing_file(name_file: str,
+                    mode: str = 'r',
+                    text_for_write: str = '') -> Union[list, int]:
+    """
+    Функция возвращая содержимое файла или записывает данные в файл
+    :param name_file: имя файла
+    :param mode: режим открытия файла
+    :param text_for_write: текст для записи в файл
+    :return: 1 - успешное выполнение функции, иначе 0
+    """
     try:
-        with open('file_with_login_pass.txt', encoding='utf-8') as file:
-            body_file = file.readlines()
-    except FileNotFoundError:
-        print('Ошибка авторизации: файл с данными не найден.')
-        return -1
-    except PermissionError:
-        print('Ошибка авторизации: недостаточно прав для чтения файла.')
-        return -1
-
-    for line in body_file:
-        if user_login == line.split()[0] and user_paswd == line.split()[1]:
-            print('Авторизация выполнена успешно!')
+        with open(name_file, mode, encoding='utf-8') as file:
+            if mode == 'r':
+                body_file = file.readlines()
+                return body_file
+            file.write(f'{text_for_write}\n')
             return 1
-
-    print('Логин или пароль не найдены или указаны неверно.\n')
-    repeat = input('Введите 1, если хотите попробовать ещё раз: ')
-    if repeat == '1':
-        get_authorization()
-    else:
-        return 0
-
-
-def get_login_verification(login: str):
-    try:
-        with open('file_with_login_pass.txt', encoding='utf-8') as file:
-            body = file.readlines()
     except FileNotFoundError:
-        print('Ошибка верификации: файл с данными не найден.')
+        print('Ошибка: файл с данными не найден.')
         return 0
     except PermissionError:
-        print('Ошибка верификации: недостаточно прав для чтения файла.')
+        print('Ошибка: недостаточно прав для чтения файла.')
         return 0
 
-    for i in body:
-        if i.split()[0] == login:
-            print(f'Логин {login} уже занят.')
-            return 0
 
+def error_details(chek_name: str, chek_data: str,
+                  min_limit: int, max_limit: int) -> int:
+    """
+    Функция проверяет переданный параметр на соответствие критериям:
+    :param chek_name: имя проверяемого параметра
+    :param chek_data: проверяемый параметр
+    :param min_limit: минимальная длина параметра
+    :param max_limit: максимальная длина параметра
+    :return: 1 - параметр соответствует критериям,
+             0 - возвращается перечень несоответствий
+    """
     chek_num, chek_alf = 0, 0
-    for i in login:
+    for i in chek_data:
         if i.isnumeric():
             chek_num += 1
         elif i.isalpha():
             chek_alf += 1
 
     err_detls = ''
-    log_len = len(login)
-    if log_len < 3:
-        err_detls = '- cлишком короткий логин\n'
-    elif log_len > 20:
-        err_detls = '- cлишком длинный логин\n'
+    chek_data_len = len(chek_data)
+    if chek_data_len < min_limit:
+        err_detls = f'- cлишком короткий {chek_name}\n'
+    elif chek_data_len > max_limit:
+        err_detls = f'- cлишком длинный {chek_name}\n'
 
     if chek_num == 0 and chek_alf == 0:
-        err_detls += '- логин не содержит ни одну букву или цифру.\n'
-
-    if log_len != chek_num + chek_alf:
-        err_detls += '- логин содержит недопустимые символы:\n'
+        err_detls += f'- {chek_name} не содержит ни одну букву или цифру.\n'
+    if chek_data_len != chek_num + chek_alf:
+        err_detls += f'- {chek_name} содержит недопустимые символы:\n'
 
     if err_detls != '':
-        print(f'Логин "{login}" не соответствует критериям:\n{err_detls}')
+        print(f'{chek_name.capitalize()} "{chek_data}" противоречит критериям:'
+              f'\n{err_detls}')
         return 0
-    else:
-        return 1
+    return 1
 
 
-def creat_login():
+def login_verificat(login: str, creat_log: bool = False) -> int:
+    """
+    Функция проверяет логин на соответствие критериям при разных ситуациях:
+    - при регистрации - проверка на уникальность, длину и допустимые символы
+    - при авторизации - только на длину и допустимые символы
+        :param login: логин для проверки на соответствие критериям
+        :param creat_log: регистрация логина (True) или авторизация (False)
+        :return: 1 - верификация прошла успешно, 0 - есть ошибки
+    """
+    if creat_log:
+        body_lines = processing_file(DATA_FILE)
+        if body_lines == 0:
+            return 0
+        for line in body_lines:
+            if login == line.split()[0]:
+                print(f'Логин {login} уже занят.')
+                return 0
+
+    if error_details('логин', login, 3, 20) == 0:
+        return 0
+
+    return 1
+
+
+def passwd_verificat(passwd: str) -> int:
+    """
+    Функция верифицирует пароль вводимый пользователем по критериям:
+        - длина пароля, содержание букв, цифр и недопустимых символов.
+
+    :param passwd: пароль который ввёл пользователь
+    :return: 1 - верификация прошла успешно, 0 - есть ошибки.
+    """
+
+    if error_details('пароль', passwd, 4, 32) == 0:
+        return 0
+
+    return 1
+
+
+def get_authorization() -> int:
+    """
+    Функция выполняем авторизацию пользователя.
+        :return: 1 - авторизация успешно выполнена, иначе 0
+    """
+    print('Вызвана функция 1: авторизация\n')
+
+    login = get_user_data('Введите ваш логин')
+    passwd = get_user_data('Введите ваш пароль')
+    verif_login = login_verificat(login)
+    verif_passwd = passwd_verificat(passwd)
+    if verif_login == 0 or verif_passwd == 0:
+        repeat = input(REQUEST_REENTER_DATA)
+        if repeat == '1':
+            get_authorization()
+        return 0
+
+    body_lines = processing_file(DATA_FILE)
+    if body_lines != 0:
+        for line in body_lines:
+            if login == line.split()[0] and passwd == line.split()[1]:
+                print('Авторизация успешно выполнена!')
+                return 1
+
+    print('Авторизация не выполнена.')
+    return 0
+
+
+def creat_login() -> Union[str, int]:
+    """
+    Функция создаёт новый логин.
+        :return: 1 - создание логина успешно выполнено, иначе 0
+    """
     while True:
         new_login = input(
             'Придумайте логин подходящий под следующие критерии:\n'
@@ -136,49 +201,20 @@ def creat_login():
             '  2) использованы только буквы и/или цифры\n'
             'Введите новый логин здесь: ')
 
-        if new_login == '':
-            new_login = input('Попробуйте ещё раз: ')
-            if new_login == '':
-                print('Ошибка: введён пустой логин\n')
-        else:
-            if get_login_verification(new_login) == 1:
-                print('Прекрасный логин!\n')
-                return new_login
+        if login_verificat(new_login, True) == 1:
+            print('Прекрасный логин!\n')
+            return new_login
 
-            repeat = input('Введите 1, чтобы попробовать ещё раз: ')
-            if repeat != '1':
-                return 0
+        repeat = input(REQUEST_REENTER_DATA)
+        if repeat != '1':
+            return 0
 
 
-def get_new_passwd_verification(passwd: str):
-    chek_num, chek_alf = 0, 0
-    for i in passwd:
-        if i.isnumeric():
-            chek_num += 1
-        elif i.isalpha():
-            chek_alf += 1
-
-    err_detls = ''
-    passwd_len = len(passwd)
-    if passwd_len < 4:
-        err_detls = '- cлишком короткий пароль\n'
-    elif passwd_len > 32:
-        err_detls = '- cлишком длинный пароль\n'
-
-    if chek_num == 0 and chek_alf == 0:
-        err_detls += '- пароль не содержит ни букв ни цифр.\n'
-
-    if passwd_len != chek_num + chek_alf:
-        err_detls += '- пароль содержит недопустимые символы:\n'
-
-    if err_detls != '':
-        print(f'Пароль "{passwd}" не соответствует критериям:\n{err_detls}')
-        return 0
-    else:
-        return 1
-
-
-def creat_passwd():
+def creat_passwd() -> Union[str, int]:
+    """
+    Функция создаёт новый пароль.
+        :return: 1 - создание логина успешно выполнено, иначе 0
+    """
     while True:
         new_passwd = input(
             'Придумайте пароль подходящий под следующие критерии:\n'
@@ -186,21 +222,36 @@ def creat_passwd():
             '  2) состоит из букв и хотя бы одной цифры\n'
             'Введите новый пароль здесь: ')
 
-        if new_passwd == '':
-            new_passwd = input('Попробуйте ещё раз: ')
-            if new_passwd == '':
-                print('Ошибка: введён пустой пароль\n')
-        else:
-            if get_new_passwd_verification(new_passwd) == 1:
-                print('Прекрасный пароль!\n')
-                return new_passwd
-            else:
-                repeat = input('Введите 1, чтобы попробовать ещё раз: ')
-                if repeat != '1':
-                    return 0
+        if passwd_verificat(new_passwd) == 1:
+            print('Прекрасный пароль!\n')
+            return new_passwd
+
+        repeat = input(REQUEST_REENTER_DATA)
+        if repeat != '1':
+            return 0
 
 
-def get_registration():
+def passwd_compare(passwd: str) -> int:
+    """
+    Функция сравнивает второй ввод пароля с перым.
+        :param passwd: первый ввод пароля пользователем
+        :return: 1 - пароли совпадают, иначе 0
+    """
+    while True:
+        test_passwd = input('Введите пароль ещё раз, чтобы запомнить его: ')
+        if passwd == test_passwd:
+            return 1
+
+        repeat = input('REQUEST_REENTER_DATA')
+        if repeat != '1':
+            return 0
+
+
+def get_registration() -> int:
+    """
+    Функция регистрирует логин и пароль нового пользователя
+        :return: 1 - регистрация прошла успешно, иначе 0
+    """
     print('Вызвана функция 2: регистрация\n')
 
     new_login = creat_login()
@@ -213,27 +264,16 @@ def get_registration():
         print('Регистрация отменена пользователем: этап создание пароля.')
         return 0
 
-    while True:
-        test_passwd = input(
-            'Введите пароль ещё раз, чтобы запомнить его: ')
-        if new_paswd == test_passwd:
-            print('Всё правильно, сохряняю ваши данные ...\n')
-            break
-        else:
-            repeat = input('Введите 1, чтобы попробовать ещё раз: ')
-            if repeat != '1':
-                print('Регистрация отменена пользователем: разные пароли')
-                return 0
+    if not passwd_compare(new_paswd):
+        print('Регистрация отменена пользователем: разные пароли')
 
-    try:
-        with open('file_with_login_pass.txt', 'a', encoding='utf-8') as file:
-
-            file.write(f'{new_login} {new_paswd}')
-        print('Регистрация выполнена успешно!')
-        return 1
-    except PermissionError:
-        print('Ошибка регистрации: недостаточно прав для работы с файлом.')
+    if not processing_file(DATA_FILE,
+                           'a',
+                           f'{new_login} {new_paswd}'):
         return 0
+
+    print('Регистрация выполнена успешно!')
+    return 1
 
 
 if __name__ == '__main__':
